6b7d31b42d99e47bf567a5cc8fd1b1198cab64a6
Bo Pace <bopace@gmail.com>
2019-08-27 21:25:33 -0600

    add basic express api server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+require('dotenv').config()
+
+const app = express()
+
+app.get('/public', function(req, res) {
+  res.json({
+    message: 'Hello from a public api!',
+  })
+})
+
+app.listen(3001)
+console.log('api server listening on ' + process.env.REACT_APP_AUTH0_AUDIENCE)
