8277e6d618afa2903f8913b2220b64b1c3dd4a37
Bo Pace <bopace@gmail.com>
2019-08-27 22:15:54 -0600

    add private api endpoint and fetch from it

--- server.js
+++ server.js
@@ -1,11 +1,37 @@
 const express = require('express')
 require('dotenv').config()
+const jwt = require('express-jwt')
+const jwksRsa = require('jwks-rsa')
+
+const checkJwt = jwt({
+  // dynamically provide a signing key based on the kid in the header
+  // and the signing keys provided by the jwks endpoint
+  secret: jwksRsa.expressJwtSecret({
+    cache: true, // cache the signing key
+    rateLimit: true,
+    jwksRequestsPerMinute: 5, // prevent attackers from requesting more than 5 per minute
+    jwksUri: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/.well-known/jwks.json`,
+  }),
+
+  // validate the audience and the issuer
+  audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+  issuer: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/`,
+
+  // this must match the algorithm selected in the auth0 dashboard under your app's advanced settings under the oauth tab
+  algorithms: ['RS256'],
+})
 
 const app = express()
 
 app.get('/public', function(req, res) {
   res.json({
     message: 'Hello from a public api!',
+  })
+})
+
+app.get('/private', checkJwt, function(req, res) {
+  res.json({
+    message: 'Hello from a private api!',
   })
 })
 
