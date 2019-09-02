295b8d5c0c3db47ccd9a55255df17b27aca8f0e8
Bo Pace <bopace@gmail.com>
2019-09-02 14:01:05 -0600

    add authentication with auth0 scopes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 require('dotenv').config()
 const jwt = require('express-jwt')
 const jwksRsa = require('jwks-rsa')
+const checkScope = require('express-jwt-authz')
 
 const checkJwt = jwt({
   // dynamically provide a signing key based on the kid in the header
@@ -32,6 +33,15 @@ app.get('/public', function(req, res) {
 app.get('/private', checkJwt, function(req, res) {
   res.json({
     message: 'Hello from a private api!',
+  })
+})
+
+app.get('/course', checkJwt, checkScope(['read:courses']), function(req, res) {
+  res.json({
+    courses: [
+      { id: 1, title: 'Building Apps with React and Redux' },
+      { id: 2, title: 'Creating Reusable React Components' },
+    ],
   })
 })
 
