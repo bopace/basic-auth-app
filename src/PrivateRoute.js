2d2d694c0226bb4edb50a3f0864f47b87c7cac8b
Bo Pace <bopace@gmail.com>
2019-09-02 15:07:05 -0600

    create and use AuthContext to refactor App.js

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -1,35 +1,39 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 import PropTypes from 'prop-types'
+import AuthContext from './AuthContext'
 
-function PrivateRoute({ component: Component, auth, scopes, ...rest }) {
+function PrivateRoute({ component: Component, scopes, ...rest }) {
   return (
-    <Route
-      {...rest}
-      render={props => {
-        // redirect to login if not logged in
-        if (!auth.isAuthenticated()) return auth.login()
+    <AuthContext.Consumer>
+      {auth => (
+        <Route
+          {...rest}
+          render={props => {
+            // redirect to login if not logged in
+            if (!auth.isAuthenticated()) return auth.login()
 
-        // display message if user lacks requierd scope(s)
-        if (scopes.length > 0 && !auth.userHasScopes(scopes)) {
-          return (
-            <h1>
-              Unauthorized - You need the following scope(s) to view this page:{' '}
-              {scopes.join(',')}.
-            </h1>
-          )
-        }
+            // display message if user lacks requierd scope(s)
+            if (scopes.length > 0 && !auth.userHasScopes(scopes)) {
+              return (
+                <h1>
+                  Unauthorized - You need the following scope(s) to view this
+                  page: {scopes.join(',')}.
+                </h1>
+              )
+            }
 
-        // render component
-        return <Component auth={auth} {...props} />
-      }}
-    />
+            // render component
+            return <Component auth={auth} {...props} />
+          }}
+        />
+      )}
+    </AuthContext.Consumer>
   )
 }
 
 PrivateRoute.propTypes = {
   component: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired,
   scopes: PropTypes.array,
 }
 
